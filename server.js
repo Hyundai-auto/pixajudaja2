08fcabf47fe6acd783bbfaa7e4a8dd0e997bb2d9
Hyundai-auto <comercial12.bra@gmail.com>
2026-04-20 22:27:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ try {
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const CAMPAIGN_ID = process.env.CAMPAIGN_ID || "133622";
+const CAMPAIGN_ID = process.env.CAMPAIGN_ID || "135605";
 
 // Armazenamento simples de cookies
 const cookieStore = {};
